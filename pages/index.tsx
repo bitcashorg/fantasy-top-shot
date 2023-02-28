87fd6b5a4aa78fc601fbdbd41126fb845f96b38f
Roberto Lucas <andler.dev@gmail.com>
2023-02-28 01:12:04 -0600

    fix: typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
     <>
       <span className="fixed blur-[200px] w-[600px] h-[600px] rounded-full top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 bg-gradient-to-tl from-red-600/20 to-violet-600/20 dark:from-red-600/40 dark:to-violet-600/40"></span>
 
-      {!open && <HomeWizard />}
+      {open && <HomeWizard />}
 
       {/* TODO: WIP... */}
       <video autoPlay width={200} height={200}>
