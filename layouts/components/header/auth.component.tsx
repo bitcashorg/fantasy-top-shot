4d606645015b0458532b474550a7ff3161bf129c
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-28 19:04:45 -0600

    chore: remove dead code

--- layouts/components/header/auth.component.tsx
+++ layouts/components/header/auth.component.tsx
@@ -2,12 +2,11 @@ import { Button } from '~/components/button'
 import { useAuthContext } from '~/context/auth.context'
 
 export const Auth = () => {
-  const { currentUser, logOut, logIn, signUp } = useAuthContext()
+  const { currentUser, logOut, logIn } = useAuthContext()
 
   const AuthedState = () => {
     return (
       <div>
-        {currentUser?.addr}
         <button
           onClick={logOut}
           className="px-4 py-2 m-2 text-gray-700 transition duration-500 bg-gray-200 border border-gray-200 rounded-md select-none ease hover:bg-gray-300 focus:outline-none focus:shadow-outline"
@@ -22,7 +21,6 @@ export const Auth = () => {
     return (
       <div className="flex gap-2">
         <Button onClick={logIn} label="Log in" variant="primary" />
-        <Button onClick={signUp} label="Sign Up" />
       </div>
     )
   }
