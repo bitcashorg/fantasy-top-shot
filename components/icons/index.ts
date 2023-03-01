c4b7ffa10094be26a4d8284ed408b7dfc6d7e66f
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-28 21:28:29 -0600

    wip: logo

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,4 +1,4 @@
 export * from './back-icon.component'
 export * from './send-prompt-icon.component'
-export * from './wizard-logo.component'
+export * from './app-logo.component'
 export * from './pwa-download-icon.component'
--- icons/index.ts
+++ icons/index.ts
@@ -1,4 +1,3 @@
 export * from './back-icon.component'
 export * from './send-prompt-icon.component'
-export * from './app-logo.component'
 export * from './dollar-icon.component'
