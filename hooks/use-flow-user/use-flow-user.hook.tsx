9d7aa7514aa1b954083e1a5236b2ea72698da28e
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-28 21:16:58 -0600

    wip: login

--- hooks/use-flow-user/use-flow-user.hook.tsx
+++ hooks/use-flow-user/use-flow-user.hook.tsx
@@ -14,7 +14,7 @@ export function useFlowUser() {
       .put('discovery.authn.endpoint', process.env.NEXT_PUBLIC_DISCOVERY_AUTH)
       .put('app.detail.icon', process.env.NEXT_PUBLIC_FLOW_APP_ICON)
       // use pop instead of default IFRAME/RPC option for security enforcement
-      .put('discovery.wallet.method', 'IFRAME/RPC')
+      .put('discovery.wallet.method', process.env.NEXT_PUBLIC_WALLET_METHOD)
       .put('app.detail.title', process.env.NEXT_PUBLIC_FLOW_APP_TITLE)
     // Sets flowUser whenver it changes
     fcl.currentUser.subscribe(setFlowUser)
